d687d282a450d601325065bb73b2c3e5b55ace78
Natitatalia <nati.mius@gmail.com>
2023-10-20 00:08:05 -0500

    actualizacion

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Mi Espacio</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>hola</title>
 </head>
 <body>
-    <h1>Hola Mundo</h1>
-    <h2>primer cambio</h2>
+    <h1>hola</h1>
 </body>
 </html>
